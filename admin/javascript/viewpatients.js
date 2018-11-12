8cd3001730d0060dd42f5620ec37c6e7b35435a7
Okundolor Mondris <justmondris@gmail.com>
2018-11-12 04:51:26 +0100

    my second commit

--- admin/javascript/viewpatients.js
+++ admin/javascript/viewpatients.js
@@ -1,65 +1,74 @@
-$(document).ready(function(){
+$(document).ready(function(){ 
 	var edit = "edit";
 	var del = "delete";
-	$("#viewpatients").click(function(){
-			$.ajax({
+	$("#viewpatients").click(function(){ 
+		$.ajax({					
 			url:"http://localhost:3000/users",
 			type:"Get",
 			dataType:"json",
-			success:function(data){
+			// contentType: "application/json"
+			success:function(data){   
 				// iterate over the data using jquery .each method
-				$(data).each(function(index, value){
+				$(data).each(function(index, value){ 
 					// use each sub object to form a complete row of table data
-					var username = value["Username"];
-					var record = "<tr><td>" + value["id"]  + "</td><td>" + value["Username"] + "</td><td>" +
-					value["First Name"] + "</td><td>" + value["Last Name"] + "</td><td>" + value["Gender"] +
-					 "</td><td>" +	value["Phone No"] + "</td><td>" + value["E-mail"] + "</td><td>" +
-					    value["L.G.A"] + "</td><td>" + value["State"]  + "</td><td>" + `<span class="${edit}"  value="${username}">&#x270e;</span>` +
-					    "</td><td>" + `<span class="${del}" value="${username}">&#x274C;</span>` + "</td><tr>"
-				     $("table").append(record)
-					})
-					$(".edit").click(function(){
-					var username = $("this").attr("value");
-					
-					$(".delete").click(function(){
+					var $row = $(`
+							<tr>
+								<td>${value.id}</td>
+								<td>${value.Username}</td>
+								<td>${value['First Name']}</td>
+								<td>${value['Last Name']}</td>
+								<td>${value.Gender}</td>
+								<td>${value['Phone No']}</td>
+								<td>${value['E-mail']}</td>
+								<td>${value['L.G.A']}</td>
+							 	<td>${value.State}</td>
+								<td><span class="edit" value="${value.Username}">&#x270e;</span></td>
+								<td><span class="delete" value="${value.Username}">&#x274c;</span></td>
+							</tr>`);
+					        $("table").append($row);
+					        $row.find('.edit').click(function() {
+					          console.log('edit clicked', value.Username);
+					        })
+
+					$(".delete").click(function(){  
 						deleteRecord(username); // delete record
 					})
+			
 				})
-
 			}
-		})
+		}) 
 	})
 
-   function deleteRecord(username){
-   		    $.ajax({
-			url:"http://localhost:3000/users",
-			type:"DELETE",
-			data:{
-				Username: "username",
-			}
-			dataType:"json",
-			success: function(data){
-				// alert("Record Deleted Sucessfully")
-			}
+   // function deleteRecord(username){
+   // 		    $.ajax({
+			// url:"http://localhost:3000/users",
+			// type:"DELETE",
+			// data:{
+			// 	Username: "username",
+			// },
+			// dataType:"json",
+			// success: function(data){
+			// 	// alert("Record Deleted Sucessfully")
+			// }
 
-   		})
+   // 		})
 
-   		 $.ajax({
-			url:"http://localhost:3000/checkin",
-			type:"DELETE",
-			data:{
-				Username: "username",
-			}
-			dataType:"json",
-			success: function(data){
-				alert("Record Deleted Sucessfully")
-			}
+   // 		$.ajax({
+			// url:"http://localhost:3000/checkin",
+			// type:"DELETE",
+			// data:{
+			// 	Username: "username",
+			// }
+			// dataType:"json",
+			// success: function(data){
+			// 	alert("Record Deleted Sucessfully")
+			// });
 
-        })
+   //      });
 
    
-    }
+   //  }
 
 	
-})
+})//
 
